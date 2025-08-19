64b2e03eaba4168be40d6847841c6358228b8097
pannawatr <pannawatr@outlook.com>
2025-08-19 16:09:12 +0700

    Add plot_grid

--- pyquity/plot.py
+++ pyquity/plot.py
@@ -1,1 +1,13 @@
+import osmnx as ox
+import networkx as nx
 import matplotlib.pyplot as plt
+
+def plot_grid(grid, ax=None, linewidth: float=0.5, figsize: tuple[float, float]=(10, 10), legend: bool=True, show: bool=True, **kwargs):
+    if ax is None:
+        fig, ax = plt.subplots(figsize=figsize)
+    ax = grid.plot(ax=ax, edgecolor='k', color='white', linewidth=linewidth, legend=legend, **kwargs)
+
+    if show:
+        plt.show()
+
+    return ax
